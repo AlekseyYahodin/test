096418edd4d5f825e17ecb44cf086d98ca77e1db
AlekseyYahodin <aleksey.yahodin@gmail.com>
2018-07-07 16:03:13 +0300

    Added basic frondend functionality

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const app = require('./src');
 const config = require('config');
+const express = require('express');
+const app = require('./src');
+
 const port = config.port;
+
+app.use(express.static(__dirname + '/frontend'));
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
 
 app.listen(port);
 console.log(`Server is listening on port ${port}`);
